7f14aac14bbe5864326749a84518691cae01f5b4
kipperz <kip@kipperz.gg>
2023-08-12 21:20:07 -0700

    test

--- cogs/log_messages.py
+++ cogs/log_messages.py
@@ -79,6 +79,7 @@ class MessageLog(commands.Cog):
 
             if moderator is None:
                 async for entry in message.guild.audit_logs(limit=5, action=discord.AuditLogAction.message_bulk_delete):
+                    print(entry.target)
                     if entry.target.id == message.author.id and entry.extra.channel.id == message.channel.id:
                         moderator = entry.user
                         break
