6ab829c03a172ce4dc86b26be4514c43e36e0002
kipperz <kip@kipperz.gg>
2023-08-15 01:14:09 -0700

    version 0.9.2

--- config/settings.py
+++ config/settings.py
@@ -1,10 +1,3 @@
-from pymongo import MongoClient
-
-from config.secrets import MONGO_URI
-
 COMMAND_PREFIX = '%'
-
-DEV_CHANNEL = 1138601380374904964
-
-mongo = MongoClient(MONGO_URI)
-log_database = mongo['logs']
+DEV_CHANNEL = 'channel_id'
+APPLICATION_ID = 'bot_application_id'
