38a3b123e5c274cccbca0eb9d4cb1de37b4e29e1
kipperz <kip@kipperz.gg>
2023-08-19 17:12:59 -0700

    Logs for each guild to separate databases

--- ext/database.py
+++ ext/database.py
@@ -5,8 +5,8 @@ from pymongo import DESCENDING, MongoClient
 from config.secrets import MONGO_URI
 from ext.audit_log import recursive_object_to_dict
 
-mongo = MongoClient(MONGO_URI)
-log_database = mongo['logs']
+
+log_database = MongoClient(MONGO_URI)
 
 def database_insert_message(message: discord.Message):
     doc = {
@@ -21,26 +21,26 @@ def database_insert_message(message: discord.Message):
         'deleted': False,
     }
 
-    result = log_database[str(message.channel.id)].insert_one(doc)
+    result = log_database[str(message.guild.id)][str(message.channel.id)].insert_one(doc)
     if result.inserted_id is not None and result.acknowledged is True:
         pass
     else:
         pass # error logger
 
 def database_insert_audit_log_entry(entry: discord.AuditLogEntry):
-    result = log_database.audit_log.insert_one(recursive_object_to_dict(entry))
+    result = log_database[str(entry.guild.id)].audit_log.insert_one(recursive_object_to_dict(entry))
     if result.inserted_id is not None and result.acknowledged is True:
         pass
     else:
         print('new log not inserted')
 
 def database_delete_message(message: discord.Message or classmethod): # Marks message as deleted in the message docs
-    result = log_database[str(message.channel.id)].update_many({'message_id': str(message.id)}, {'$set': {'deleted': True}})
+    result = log_database[str(message.guild.id)][str(message.channel.id)].update_many({'message_id': str(message.id)}, {'$set': {'deleted': True}})
     if result is None:
         pass # error logger
 
 async def database_get_last_message(bot: commands.Bot, guild_id: int, channel_id: int, message_id: int):
-    message_doc = log_database[str(channel_id)].find_one({'message_id': str(message_id)}, sort=[('edited_at', DESCENDING)])
+    message_doc = log_database[str(guild_id)][str(channel_id)].find_one({'message_id': str(message_id)}, sort=[('edited_at', DESCENDING)])
     if message_doc is None:
         message_doc = {}
         class message_author: #pylint: disable=invalid-name
