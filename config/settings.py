8b6eed98a0bbc3d152f63676c5ae2db1615aa253
kipperz <kip@kipperz.gg>
2024-06-26 10:44:41 -0700

    Add comments

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,18 @@
 COMMAND_PREFIX = '^'
-DEV_CHANNEL = 1234567890
+
 APPLICATION_ID = 'bot_application_id'
-DEVELOPER_IDS = [] # list of int
+
+# Channel ID (int) for developer alerts
+DEV_CHANNEL = 1234567890
+
+# List of discord IDs (int) to access developer commands
+DEVELOPER_IDS = []
+
+# Whether the bot should be invisible or not
 INVISIBLE_STATUS = False
-CUSTOM_STATUS = [] # list of str
-CUSTOM_STATUS_DELAY = 5 # seconds
+
+# Set the bot status to the following list of str
+CUSTOM_STATUS = []
+
+# If more than one string, rotate every ## second
+CUSTOM_STATUS_DELAY = 5
