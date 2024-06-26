1705d048635984750f3b936dab5f78de4aa29ecc
kipperz <kip@kipperz.gg>
2024-06-25 19:34:34 -0700

    Revert

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import os
 import sys
-
-SECRETS_PATH = 'config/secrets.py'
-
-if not os.path.exists(SECRETS_PATH):
-    os.makedirs('config', exist_ok=True)
-    with open(SECRETS_PATH, 'w', encoding='utf-8') as f:
-        f.write("TOKEN = ''\nMONGO_URI = ''\n")
-
-    print(f'Created "{SECRETS_PATH}". Please set the TOKEN and MONGO_UR variables')
-    sys.exit(1)
 
 from config.secrets import TOKEN
 from ext.discord_bot import DiscordBot
