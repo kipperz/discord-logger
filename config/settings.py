1f06be6a578da7fa20d3542b81ca50025bd02459
Kip <103238779+kipperz@users.noreply.github.com>
2024-06-25 18:40:07 -0700

    Add descriptions

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 COMMAND_PREFIX = '^'
 DEV_CHANNEL = 1234567890
 APPLICATION_ID = 'bot_application_id'
-DEVELOPER_IDS = []
+DEVELOPER_IDS = [] # list of int
 INVISIBLE_STATUS = False
-CUSTOM_STATUS = []
+CUSTOM_STATUS = [] # list of str
